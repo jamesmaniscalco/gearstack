e08b2a45ef020f25b32a252bddff389289a6b961
James Maniscalco <maniscalco.jt@gmail.com>
2013-05-03 20:49:41 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gearstack::Application.config.secret_token = '81a67573debaa447edeeabdfc905a022c963152c2bb36c5648b07e5b5ba2c9d684ec692150afaa44f6a9377b4775353d8cf58b3c794cedd76449648c93891c59'
