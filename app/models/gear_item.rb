3dd1d25c38f3dcd746138fe15c994c978ca30b3d
James Maniscalco <maniscalco.jt@gmail.com>
2013-05-05 17:29:24 -0400

    made gear controller; copied from ember branch, hopefully there's no JS left over

--- app/models/gear_item.rb
+++ app/models/gear_item.rb
@@ -4,4 +4,10 @@ class GearItem < ActiveRecord::Base
   #relations
   belongs_to :owner, :class_name => "User"
   belongs_to :possessor, :class_name => "User"
+
+  #validations
+  validates :name, :presence => true
+  validates :status, :presence => true
+  validates :weight, :numericality => true
+  validates :status, :inclusion => { :in => %w(checkedin checkedout)}
 end
