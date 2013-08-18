b183b668b253ae5ac73e94fb6e8c6a418a3ec26a
James Maniscalco <maniscalco.jt@gmail.com>
2013-08-18 17:54:17 +0200

    removed vendor/plugins, made the initialize_on_precompile false for Heroku

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,8 @@ module Gearstack
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    # but prevent initializing the application when things aren't available (for Heroku)
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
